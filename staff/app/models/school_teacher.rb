4b185b4f0d7492e635deeb45baadbca9c59e179b
Charitakis Yannis <github@ych.gr>
2017-09-02 07:29:41 +0300

    Μορφοποιήσεις και μεταφράσεις

--- staff/app/models/school_teacher.rb
+++ staff/app/models/school_teacher.rb
@@ -1,4 +1,10 @@
 class SchoolTeacher < ApplicationRecord
   belongs_to :teacher
   belongs_to :school
+
+  def hours_for_teaching
+    disposal_duration < teacher.teaching_hours ?
+      disposal_duration : teacher.teaching_hours
+  end
+
 end
