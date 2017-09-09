b9d752a1aabce58d989e23961a7520a5fd321614
Charitakis Yannis <github@ych.gr>
2017-09-09 16:16:18 +0300

    Messages improved

--- staff/app/models/person.rb
+++ staff/app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ApplicationRecord
+
+  def full_name
+    "#{last_name} #{first_name} του #{father_name}"
+  end
+
 end
--- staff/app/models/school_course.rb
+++ staff/app/models/school_course.rb
@@ -4,24 +4,4 @@ class SchoolCourse < ApplicationRecord
 
   has_many :school_course_teachers
 
-  def get_required_hours
-    if lesson.description.include? "Πληροφορική" or 
-        lesson.description.include? "Ερευνητική"
-      if capacity > 17
-        return 2 * lesson.hours
-      end
-    end
-    return lesson.hours
-  end
-
-  def get_required_no_teachers
-    if lesson.lesson_type.code == 'Ε' #TODO fragile
-      if capacity > 12
-        return 2
-      end
-    end
-
-    return 1
-  end
-
 end
