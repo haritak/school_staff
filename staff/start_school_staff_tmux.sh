e079954ae36c34d25b847ef1a5169c6996261ad7
administrator@srv-1tee-moiron <administrator@srv-1tee-moiron>
2017-09-12 11:41:20 +0300

    Added script to start staff

---
+++ staff/start_school_staff_tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+tmux new-session -s staff -d
+tmux send-keys -t staff 'cd /home/administrator/school_staff/staff' C-m
+tmux send-keys -t staff 'bin/rails server -b 0.0.0.0 -p 4000' C-m
+tmux new-window -t staff
