bd6377009c076e451e0a6120452edf084820f0bb
Charitakis Yannis <github@ych.gr>
2017-10-11 06:11:02 +0300

    start in the right folder

--- staff/start_school_staff_tmux.sh
+++ staff/start_school_staff_tmux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd ~/school_staff/staff
 tmux new-session -s staff -d
 tmux send-keys -t staff 'cd ~/school_staff/staff' C-m
 tmux send-keys -t staff 'bin/rails server -b 0.0.0.0 -p 4000' C-m
