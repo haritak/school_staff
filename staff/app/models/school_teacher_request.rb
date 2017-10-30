5c1947b8f73157dcc77daea2ad70a49b6901b575
Charitakis Yannis <github@ych.gr>
2017-10-30 18:36:56 +0200

    Show only requests not having a response

--- staff/app/models/school_teacher_request.rb
+++ staff/app/models/school_teacher_request.rb
@@ -2,6 +2,8 @@ class SchoolTeacherRequest < ApplicationRecord
   belongs_to :school_teacher
   belongs_to :request_specification
 
+  
+
   def description
     "#{school_teacher.short_s} "+
       "(#{school_teacher.teacher.teacher_working_class.description}) #{request_specification.description} " +
@@ -9,6 +11,12 @@ class SchoolTeacherRequest < ApplicationRecord
   end
 
   def self.open_cases
-    SchoolTeacherRequest.order(:id)
+    #TODO : There should be a more portable way of doing this :
+    SchoolTeacherRequest.find_by_sql("select * from school_teacher_requests " +
+                                     "where id not in " + 
+                                     "(select school_teacher_requests.id from " +
+                                     "school_teacher_requests join " + 
+                                     "school_teacher_responses on " +
+                                     "school_teacher_requests.id=school_teacher_responses.school_teacher_request_id)")
   end
 end
