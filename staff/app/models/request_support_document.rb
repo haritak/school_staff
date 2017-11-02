6d63021da1222ca7df5a2aac532c932e3f5ea004
Charitakis Yannis <github@ych.gr>
2017-11-02 07:26:03 +0200

    gui things

--- staff/app/models/request_support_document.rb
+++ staff/app/models/request_support_document.rb
@@ -2,6 +2,11 @@ class RequestSupportDocument < ApplicationRecord
   belongs_to :school_teacher_request
 
   def to_s
-    "Την από #{issuer_date.strftime "%d-%m-%Y" } και με αρ.πρωτ. #{issuer_protocol} #{description} του #{issuer}."
+    issuer_protocol_str = ""
+    if issuer_protocol!=nil and
+        issuer_protocol.strip != ''
+      issuer_protocol_str = "και με αρ.πρωτ. #{issuer_protocol}"
+    end
+    "Την από #{issuer_date.strftime "%d-%m-%Y" } #{issuer_protocol_str} #{description} του #{issuer}."
   end
 end
