ed1f0915705b46bb975ca62984fcb460a42f80c9
Charitakis Yannis <github@ych.gr>
2017-11-10 06:34:15 +0200

    Easier description of support documents

--- staff/app/models/request_support_document.rb
+++ staff/app/models/request_support_document.rb
@@ -8,6 +8,6 @@ class RequestSupportDocument < ApplicationRecord
       issuer_protocol_str = "και με αρ.πρωτ. #{issuer_protocol}"
     end
     #start with two spaces to align with previous numbered list.
-    "  Την από #{issuer_date.strftime "%d-%m-%Y" } #{issuer_protocol_str} #{description} του #{issuer}."
+    "  Την από #{issuer_date.strftime "%d-%m-%Y" } #{issuer_protocol_str} #{description} #{issuer}."
   end
 end
