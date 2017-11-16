239e3f8aea4e43a165e1b122c4089a5f33903802
Charitakis Yannis <github@ych.gr>
2017-11-16 05:41:18 +0200

    GUI changes

--- staff/app/mailers/staff_mailer.rb
+++ staff/app/mailers/staff_mailer.rb
@@ -1,14 +1,16 @@
 class StaffMailer < ApplicationMailer
   def new_school_teacher_request( school_teacher_request )
     @school_teacher_request = school_teacher_request
+    settings = 
+      Setting.find_by( school: @school_teacher_request.school_teacher.school )
     person = @school_teacher_request.school_teacher.teacher.person
     starting_date = school_teacher_request.starting_date
     duration = school_teacher_request.duration
     attachments['αίτηση.odt'] = File.read( school_teacher_request.filename )
     mail(
       from: 'GeoStaff <artemis1epalmoiron@gmail.com>',
-      to: 'charitakis.ioannis@gmail.com', 
-      cc: ['bp10.charitakis@gmail.com', 'haritak@sch.gr'],
+      to: [settings.mng_email, settings.ypo1_email],
+      cc: [settings.ypo2_email, settings.adeies_email],
       subject: "Αίτηση άδειας #{person.short_name} για #{duration}, από #{starting_date}",
     )
   end
