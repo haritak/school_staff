6ca18729ffc2af23f2fe6c7774c6b2950d18e9cf
administrator@srv-1tee-moiron <administrator@srv-1tee-moiron>
2020-01-23 16:04:36 +0200

    Fixed email settings and auto startup script

--- staff/start_school_staff_tmux.sh
+++ staff/start_school_staff_tmux.sh
@@ -3,7 +3,7 @@ cd ~/school_staff/staff
 tmux new-session -s staff -d
 tmux send-keys -t staff 'cd ~/school_staff/staff' C-m
 #tmux send-keys -t staff 'bin/rails server -b 0.0.0.0 -p 4000' C-m
-tmux send-keys -t staff 'start_rails_in_production.sh' C-m
+tmux send-keys -t staff './start_rails_in_production.sh' C-m
 tmux new-window -t staff
 tmux send-keys -t staff 'mysql school_staff -u root -p' C-m
 tmux new-window -t staff
